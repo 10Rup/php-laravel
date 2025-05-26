04c196699008e7849302314c96f061fd150a1395
10Rup <rupmandal.lspl@gmail.com>
2025-05-26 17:44:56 +0530

    to do list

--- TaskManager/To-Do-List/app/Http/Controllers/UserController.php
+++ TaskManager/To-Do-List/app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function show(Request $request){
+        // return $request->input('username');
         return $request;
     }
 }
