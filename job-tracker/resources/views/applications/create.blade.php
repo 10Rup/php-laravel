7d879d203c4ebcd0d3722d7087dc28c9e1e52e6f
Rup Mandal <104366033+10Rup@users.noreply.github.com>
2025-06-02 11:46:48 +0530

    Delete job-tracker directory
    Delete job-tracker directory

--- job-tracker/resources/views/applications/create.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<h1>Create Job Application</h1>
-@include('applications.form', ['action' => route('applications.store'), 'isEdit' => false])
-@endsection
--- job-tracker/resources/views/applications/edit.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<h1>Edit Job Application</h1>
-@include('applications.form', ['action' => route('applications.update', $application), 'isEdit' => true, 'application' => $application])
-@endsection
